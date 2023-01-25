9e63b3848a430c4ea5c294a89c2bc3d42c36f6e8
zillur-rgb <zillurdeu@gmail.com>
2023-01-25 08:39:49 +0100

    created Next.js tRPC Client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools';
+import { trpc } from '~/utils/trpc';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default trpc.withTRPC(MyApp);
